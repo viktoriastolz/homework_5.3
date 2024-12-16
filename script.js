e4663c51e3fecba2a996bbf36ab7f7fe959639dc
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-16 19:14:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let input = prompt("Enter an integer N:");
 if (!input?.trim() || isNaN(input.trim()) || !Number.isInteger(Number(input.trim()))) {
   alert("Error: Please enter a valid integer!");
 } else {
-  let N = Number(input.trim());
+  const N = Number(input);
   if (N < 1) {
     alert("Error: The number must be greater than or equal to 1!");
   } else {
